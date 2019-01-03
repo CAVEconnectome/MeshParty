2a2bb3547ff2a5d3f60e9004b6479460b26ca9bc
Forrest Collman <forrest.collman@gmail.com>
2019-01-03 15:19:18 -0800

    renaming and extending vtk module

--- meshparty/trimesh_vtk.py
+++ meshparty/trimesh_vtk.py
@@ -2,7 +2,26 @@ import vtk
 from vtk.util.numpy_support import numpy_to_vtk, numpy_to_vtkIdTypeArray, vtk_to_numpy
 import numpy as np
 
-def numpy_rep_to_vtk(vertices, shapes):
+
+def numpy_to_vtk_cells(mat):
+    cells = vtk.vtkCellArray()
+
+    # Seemingly, VTK may be compiled as 32 bit or 64 bit.
+    # We need to make sure that we convert the trilist to the correct dtype
+    # based on this. See numpy_to_vtkIdTypeArray() for details.
+    isize = vtk.vtkIdTypeArray().GetDataTypeSize()
+    req_dtype = np.int32 if isize == 4 else np.int64
+    n_elems = mat.shape[0]
+    n_dim = mat.shape[1]
+    cells.SetCells(n_elems,
+                   numpy_to_vtkIdTypeArray(
+                       np.hstack((np.ones(n_elems)[:, None] * n_dim,
+                                  mat)).astype(req_dtype).ravel(),
+                       deep=1))
+    return cells
+
+
+def numpy_rep_to_vtk(vertices, shapes, edges=None):
     """ converts a numpy representation of vertices and vertex connection graph
       to a polydata object and corresponding cell array
 
@@ -15,28 +34,16 @@ def numpy_rep_to_vtk(vertices, shapes):
         and a vtkCellArray of the shapes
     """
 
-    ndim = shapes.shape[1]
-
     mesh = vtk.vtkPolyData()
     points = vtk.vtkPoints()
     points.SetData(numpy_to_vtk(vertices, deep=1))
     mesh.SetPoints(points)
 
-    cells = vtk.vtkCellArray()
+    cells = numpy_to_vtk_cells(shapes)
+    if edges is not None:
+        edges = numpy_to_vtk_cells(edges)
 
-    # Seemingly, VTK may be compiled as 32 bit or 64 bit.
-    # We need to make sure that we convert the trilist to the correct dtype
-    # based on this. See numpy_to_vtkIdTypeArray() for details.
-    isize = vtk.vtkIdTypeArray().GetDataTypeSize()
-    req_dtype = np.int32 if isize == 4 else np.int64
-    n_tris = shapes.shape[0]
-    cells.SetCells(n_tris,
-                   numpy_to_vtkIdTypeArray(
-                       np.hstack((np.ones(n_tris)[:, None] * ndim,
-                                  shapes)).astype(req_dtype).ravel(),
-                       deep=1))
-
-    return mesh, cells
+    return mesh, cells, edges
 
 
 def graph_to_vtk(vertices, edges):
@@ -64,7 +71,7 @@ def graph_to_vtk(vertices, edges):
     return mesh
 
 
-def trimesh_to_vtk(vertices, tris):
+def trimesh_to_vtk(vertices, tris, mesh_edges=None):
     """Return a `vtkPolyData` representation of a :map:`TriMesh` instance
     Parameters
     ----------
@@ -86,10 +93,18 @@ def trimesh_to_vtk(vertices, tris):
     if np.max(tris) >= len(vertices):
         msg = 'edges refer to non existent vertices {}.'
         raise ValueError(msg.format(np.max(tris)))
-    mesh, cells = numpy_rep_to_vtk(vertices, tris)
+    mesh, cells, edges = numpy_rep_to_vtk(vertices, tris, mesh_edges)
     mesh.SetPolys(cells)
+    if edges is not None:
+        mesh.SetLines(edges)
 
     return mesh
+
+
+def vtk_cellarray_to_shape(vtk_cellarray, ncells):
+    cellarray = vtk_to_numpy(vtk_cellarray)
+    cellarray = cellarray.reshape(ncells, int(len(cellarray)/ncells))
+    return cellarray[:, 1:]
 
 
 def decimate_trimesh(trimesh, reduction=.1):
@@ -103,10 +118,40 @@ def decimate_trimesh(trimesh, reduction=.1):
 
     points = vtk_to_numpy(out_poly.GetPoints().GetData())
     ntris = out_poly.GetNumberOfPolys()
-    cellarray = vtk_to_numpy(out_poly.GetPolys().GetData())
-    cellarray = cellarray.reshape(ntris, 4)
-    tris = cellarray[:, 1:4]
+    tris = vtk_cellarray_to_shape(out_poly.GetPolys().GetData(), ntris)
     return points, tris
+
+
+def remove_unused_verts(mesh):
+    used_verts=np.unique(mesh.faces.ravel())
+    new_verts=mesh.vertices[used_verts,:]
+    new_face = np.zeros(mesh.faces.shape)
+    for i in range(3):   
+        new_face[:,i]=np.searchsorted(used_verts, mesh.faces[:,i])
+    return new_verts, new_face
+
+
+def filter_largest_cc(trimesh):
+    poly = trimesh_to_vtk(trimesh.vertices, trimesh.faces, trimesh.mesh_edges)
+    connf = vtk.vtkConnectivityFilter()
+    connf.SetInputData(poly)
+    connf.SetExtractionModeToLargestRegion()
+    connf.Update()
+    clean = vtk.vtkCleanPolyData()
+    clean.SetInputConnection(connf.GetOutputPort())
+    clean.PointMergingOff()
+    clean.Update()
+
+    out_poly = clean.GetOutput()
+    points = vtk_to_numpy(out_poly.GetPoints().GetData())
+    ntris = out_poly.GetNumberOfPolys()
+    tris = vtk_cellarray_to_shape(out_poly.GetPolys().GetData(), ntris)
+    nedges = out_poly.GetNumberOfLines()
+    if nedges > 0:
+        edges = vtk_cellarray_to_shape(out_poly.GetLines().GetData(), nedges)
+    else:
+        edges = None
+    return points, tris, edges
 
 
 def calculate_cross_sections(mesh, graph_verts, graph_edges):
@@ -161,3 +206,30 @@ def calculate_cross_sections(mesh, graph_verts, graph_edges):
         cross_sections[k] = massfilter.GetSurfaceArea()
 
     return cross_sections
+
+def make_vtk_skeleton_from_paths(verts, paths):
+    cell_list =[]
+    num_cells=0
+    for p in paths: 
+        cell_list.append(len(p))
+        cell_list+=p
+        num_cells+=1
+    cell_array = np.array(cell_list)
+
+    mesh = vtk.vtkPolyData()
+    points = vtk.vtkPoints()
+    points.SetData(numpy_to_vtk(verts, deep=1))
+    mesh.SetPoints(points)
+
+    cells = vtk.vtkCellArray()
+
+    # Seemingly, VTK may be compiled as 32 bit or 64 bit.
+    # We need to make sure that we convert the trilist to the correct dtype
+    # based on this. See numpy_to_vtkIdTypeArray() for details.
+    isize = vtk.vtkIdTypeArray().GetDataTypeSize()
+    req_dtype = np.int32 if isize == 4 else np.int64
+
+    cells.SetCells(num_cells,
+                   numpy_to_vtkIdTypeArray(cell_array,deep=1))
+    mesh.SetLines(cells)
+    return mesh
