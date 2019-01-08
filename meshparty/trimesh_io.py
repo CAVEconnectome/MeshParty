a43e61472d4d1abf63616c09085747d8a0c37211
Forrest Collman <forrest.collman@gmail.com>
2019-01-08 14:54:31 -0800

    generalized filter faces to filter shapes, added filter edges

--- meshparty/trimesh_io.py
+++ meshparty/trimesh_io.py
@@ -556,37 +556,44 @@ class Mesh(trimesh.Trimesh):
                                     return_faces=return_faces,
                                     pc_norm=pc_norm)
 
-    def _filter_faces(self, node_ids):
+     def _filter_shapes(self, node_ids, shapes):
         """ node_ids has to be sorted! """
         if not isinstance(node_ids[0], list) and \
                 not isinstance(node_ids[0], np.ndarray):
             node_ids = [node_ids]
-
+        ndim = shapes.shape[1]
         if isinstance(node_ids, np.ndarray):
             all_node_ids = node_ids.flatten()
         else:
             all_node_ids = np.concatenate(node_ids)
 
-        filter_ = np.in1d(self.faces[:, 0], all_node_ids)
-        pre_filtered_faces = self.faces[filter_].copy()
-        filter_ = np.in1d(pre_filtered_faces[:, 1], all_node_ids)
-        pre_filtered_faces = pre_filtered_faces[filter_]
-        filter_ = np.in1d(pre_filtered_faces[:, 2], all_node_ids)
-        pre_filtered_faces = pre_filtered_faces[filter_]
+        filter_ = np.in1d(shapes[:, 0], all_node_ids)
+        pre_filtered_shapes = shapes[filter_].copy()
+        for k in range(1, ndim):
+            filter_ = np.in1d(pre_filtered_shapes[:, k], all_node_ids)
+            pre_filtered_shapes = pre_filtered_shapes[filter_]
 
-        filtered_faces = []
+        filtered_shapes = []
 
         for ns in node_ids:
-            f = pre_filtered_faces[np.in1d(pre_filtered_faces[:, 0], ns)]
-            f = f[np.in1d(f[:, 1], ns)]
-            f = f[np.in1d(f[:, 2], ns)]
+            f = pre_filtered_shapes[np.in1d(pre_filtered_shapes[:, 0], ns)]
+            for k in range(1, ndim):
+                f = f[np.in1d(f[:, k], ns)]
 
             f = np.unique(np.concatenate([f.flatten(), ns]),
-                          return_inverse=True)[1][:-len(ns)].reshape(-1, 3)
+                          return_inverse=True)[1][:-len(ns)].reshape(-1, ndim)
 
-            filtered_faces.append(f)
+            filtered_shapes.append(f)
 
-        return filtered_faces
+        return filtered_shapes
+
+    def _filter_faces(self, node_ids):
+        """ node_ids has to be sorted! """
+        return self._filter_shapes(node_ids, self.faces)
+
+    def _filter_mesh_edges(self, node_ids):
+        """ node_ids has to be sorted! """
+        return self._filter_shapes(node_ids, self.mesh_edges)
 
     def get_local_meshes(self, n_points, max_dist=np.inf, center_node_ids=None,
                          center_coords=None, pc_align=False, pc_norm=False,
