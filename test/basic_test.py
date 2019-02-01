e09e4b4a792fa17e470dddf0755c6aa35bf27d48
Forrest Collman <forrestc@MoFo-iMac-Pro.local>
2019-02-01 06:08:47 -0800

    adding some read write tests

--- test/basic_test.py
+++ test/basic_test.py
@@ -1,8 +1,11 @@
 from meshparty import trimesh_io
 import numpy as np
+import tempfile
+import pytest
 
 
-def test_basic_mesh():
+@pytest.fixture(scope='module')
+def basic_mesh():
 
     verts = np.array([[0,0,0],[1,0,0],[0,1,0],[1,1,0],[0,0,1]])
     faces = np.array([[0,1,2],
@@ -10,3 +13,12 @@ def test_basic_mesh():
                       [3,4,2]])
     mesh = trimesh_io.Mesh(verts, faces, process=False)
     assert np.all(mesh.vertices == verts)
+    yield mesh
+
+def test_write_mesh(basic_mesh, tmpdir):
+
+    tempfile = str(tmpdir.join('test.h5'))
+    trimesh_io.write_mesh_h5(tempfile, basic_mesh.vertices, basic_mesh.faces)
+
+    new_mesh = trimesh_io.read_mesh_h5(tempfile)
+    assert(np.all(basic_mesh.vertices == new_mesh[0]))
