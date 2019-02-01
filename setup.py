b0a27e5ce54132ed9c51ffc2e98d88cb5127be8c
Forrest Collman <forrest.collman@gmail.com>
2019-02-01 11:46:53 -0800

    Test infrastructure (#11)
    * adding pytest to setup.py
    
    * adding vscode to gitignore
    
    * adding a travis.yml
    
    * adding travis file
    
    * basic tests functioning
    
    * adding to gitignore
    
    * adding libassimp-dev
    
    * removing only 3.6 testing
    
    * add code cov token
    
    * pre install numpy
    
    * trying with no pip cache
    
    * trying to upgrade numpy
    
    * trying to install pymeshfix from scratch
    
    * removing install from source
    
    * adding some read write tests
    
    * adding pip cache back
    
    * fix typo in travis yaml

--- setup.py
+++ setup.py
@@ -2,6 +2,29 @@ from setuptools import setup
 import re
 import os
 import codecs
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import shlex
+        import pytest
+        self.pytest_args += " --cov=meshparty --cov-report html "\
+                            "--junitxml=test-reports/test.xml"
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+with open('test_requirements.txt', 'r') as f:
+    test_required = f.read().splitlines()
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -51,5 +74,7 @@ setup(
     include_package_data=True,
     install_requires=required,
     setup_requires=['pytest-runner'],
-    dependency_links=dependency_links
-)
+    dependency_links=dependency_links,
+    tests_require=test_required,
+    cmdclass={'test': PyTest},
+    )
