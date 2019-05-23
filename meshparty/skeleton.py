fixing distance calculation

meshparty/skeleton.py
@@ -393,7 +393,7 @@ class Skeleton:
     def _single_path_length(self, path):
         xs = self.vertices[path[:-1]]
         ys = self.vertices[path[1:]]
-        return np.sum(np.linalg.norm(ys-xs))
+        return np.sum(np.linalg.norm(ys-xs, axis=1))
 
     def _compute_paths(self):
         '''