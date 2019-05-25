0ec2ce6411ac04cbae2cb058843077d1a7273579
Casey Schneider-Mizell <caseysm@gmail.com>
2019-05-24 17:15:21 -0700

    Test: more skeleton_io

--- test/skeleton_io_test.py
+++ test/skeleton_io_test.py
@@ -1,5 +1,6 @@
 import tempfile
 import numpy as np
+import pandas as pd
 import pytest
 import itertools
 import contextlib
@@ -39,6 +40,29 @@ def simple_skeleton():
 
     yield skeleton.Skeleton(verts, edges, root=0)
 
+@pytest.fixture(scope='session')
+def simple_skeleton_with_properties():
+    verts = np.array([[1, 0, 5],
+                      [1, 1, 5],
+                      [1, 2, 5],
+                      [0, 2, 5],
+                      [0, 2, 4],
+                      [2, 2, 5],
+                      [2, 2, 6]])
+
+    edges = np.array([[0,1],
+                      [1,2],
+                      [2,3],
+                      [3,4],
+                      [2,5],
+                      [5,6]])
+
+    test_prop = np.arange(len(verts))
+    mesh_to_skel_map = np.arange(0, 10*len(verts), 10)
+    yield skeleton.Skeleton(verts, edges, mesh_to_skel_map=mesh_to_skel_map,
+                            vertex_properties={'test':test_prop}, root=0)
+
+
 @pytest.mark.parametrize(
     'overwrite_flag,file_exist',
     itertools.product(overwrite_flags, io_file_exists))
@@ -58,3 +82,22 @@ def test_skeleton_read_write(simple_skeleton, overwrite_flag, file_exist):
         assert np.all(skback.vertices==sk.vertices)
         assert np.all(skback.edges==sk.edges)
         assert skback.root == sk.root
+
+
+def test_skeleton_read_write_with_props(simple_skeleton_with_properties):
+    sk = simple_skeleton_with_properties
+
+    fname = 'test_sk.h5'
+    skeleton_io.write_skeleton_h5(sk, fname, overwrite=True)
+    skback = skeleton_io.read_skeleton_h5(fname)
+    assert np.all(skback.vertex_properties['test'] == sk.vertex_properties['test'])
+    assert np.all(skback.mesh_to_skel_map == sk.mesh_to_skel_map)
+
+
+def test_swc_write(simple_skeleton_with_properties):
+    sk = simple_skeleton_with_properties
+    fname = 'test_sk.swc'
+    labels = [0, 1, 1, 1, 3, 3, 3]
+    skeleton_io.export_to_swc(sk, fname, radius=sk.vertex_properties['test'], node_labels=labels, xyz_scaling=1)
+    sk_pd = pd.read_csv(fname, sep=' ', header=None, names=['index', 'type', 'x', 'y', 'z', 'r', 'parent'])
+    assert sk_pd.loc[3].parent == 2 and sk_pd.loc[3].z == 5
