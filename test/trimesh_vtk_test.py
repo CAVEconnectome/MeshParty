abce3127cc62d5831976f8847eb4a97df6cf05a8
Forrest Collman <forrest.collman@gmail.com>
2019-05-28 15:00:03 -0700

    only counting large differences in pixels

--- test/trimesh_vtk_test.py
+++ test/trimesh_vtk_test.py
@@ -5,14 +5,14 @@ import pytest
 import os
 import imageio
 
-def compare_img_to_test_file(fname, back_val = 255, close=1):
+def compare_img_to_test_file(fname, back_val = 255, close=15):
     img_test = imageio.imread(fname).astype(np.int16)
     tmpl_path = os.path.join('test/test_files/', os.path.split(fname)[1])
     img_tmpl = imageio.imread(tmpl_path).astype(np.int16)
 
     non_background = np.any((img_test != back_val) | (img_tmpl != back_val), axis=2)
     diff = np.linalg.norm(img_test-img_tmpl, axis=2)
-    perc_close = np.sum((diff > 0) & (non_background))/np.sum(non_background)
+    perc_close = np.sum((diff > close) & (non_background))/np.sum(non_background)
     assert(perc_close>.9)
 
 @contextlib.contextmanager
