14790a849a72dad0071426e30591d2971a939e0d
Forrest Collman <forrest.collman@gmail.com>
2019-05-28 15:44:24 -0700

    fix diff < close

--- test/test_mesh_filters.py
+++ test/test_mesh_filters.py
@@ -10,7 +10,7 @@ def compare_img_to_test_file(fname, back_val = 255, close=15):
 
     non_background = np.any((img_test != back_val) | (img_tmpl != back_val), axis=2)
     diff = np.linalg.norm(img_test-img_tmpl, axis=2)
-    perc_close = np.sum((diff > close) & (non_background))/np.sum(non_background)
+    perc_close = np.sum((diff < close) & (non_background))/np.sum(non_background)
     assert(perc_close>.9)
 
 def test_filter_components_by_size(full_cell_mesh):
