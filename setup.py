f1ed54849788fe2f3f76be225830b8ec43657d45
Forrest Collman <forrest.collman@gmail.com>
2019-05-31 11:54:42 -0700

    changing version scheming

--- setup.py
+++ setup.py
@@ -26,23 +26,6 @@ class PyTest(TestCommand):
 with open('test_requirements.txt', 'r') as f:
     test_required = f.read().splitlines()
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
     print(required)
@@ -60,11 +43,8 @@ for i_l in range(len(required)):
 for i_l in del_ls[::-1]:
     del required[i_l]
 
-print(dependency_links)
-print(required)
-
 setup(
-    version=find_version("meshparty", "__init__.py"),
+    use_scm_version=True,
     name='meshparty',
     description='a service to work with meshes',
     author='Sven Dorkenwald',
