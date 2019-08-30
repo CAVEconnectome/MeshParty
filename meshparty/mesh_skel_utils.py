38de1f726b157d300cc5c522657509a2896286c1
Sharmi <shtaaa@gmail.com>
2019-08-30 11:41:53 -0700

    adding tests

--- meshparty/mesh_skel_utils.py
+++ meshparty/mesh_skel_utils.py
@@ -4,28 +4,25 @@ import trimesh
 from . import utils
 
 
-def intersection(lst1, lst2): 
-    return list(set(lst1) & set(lst2)) 
 
-
-def point_to_skel_meshpath(mesh,sk,pt,filter=None):
+def point_to_skel_meshpath(mesh,sk,pt,filterpts=None):
     '''
 
     Given a mesh, a skeleton and a point,  find the path along the mesh between a point on the mesh and the skeleton (closest skeleton point). If the point is not on the mesh, the point used is the mesh vertex that is closest to the point
     :param mesh: Trimesh-like mesh with N vertices
     :param sk: a skeleton whose vertices are a subset of the vertices of mesh
     :param pt : a length 3 array specifying a point location
-    :param filter: a boolean array which is the filter that was used to generate "mesh", 
+    :param filterpts: a boolean array which is the filter that was used to generate "mesh", 
      if "mesh" is a filtered version of the mesh that was used to generate the skeleton "sk".
      If sk was generated from mesh as is, then use filter=None
     '''
 
 
-    if filter is None:
+    if filterpts is None:
     	t = sk.vertex_properties['mesh_index']
     	sk_inds = [val for i,val in enumerate(t) if not val == -1 ]
     else:
-	validinds = np.where(filtpts)[0]
+	validinds = np.where(filterpts)[0]
         localskeletoninds = list(set(sk.vertex_properties['mesh_index']) & set(validinds)) #intersection of validinds and sk.vertex_properties['mesh_index']
 	sk_inds = [i for i, val in enumerate(validinds) if val in localskeletoninds]
 
--- test/test_mesh_to_skel_utils.py
+++ test/test_mesh_to_skel_utils.py
@@ -1,12 +1,18 @@
-from meshparty import utils,mesh_to_skel_utils
+from meshparty import utils,mesh_to_skel_utils,mesh_filters
 import numpy as np
 import os
 
 
 def test_point_to_skel_meshpath(full_cell_mesh,full_cell_skeleton):
-    pt = []
-    path = mesh_to_skel_utils.point_to_skel_meshpath(full_cell_mesh,full_cell_skeleton,pt)
-    assert (1==1) 
 
+    pt = [306992, 214288, 11560]
+    filterpts = mesh_filters.filter_spatial_distance_from_points(mesh, [pt], 2000)
+    loc_mesh = mesh.apply_mask(filterpts)
+
+    path = mesh_to_skel_utils.point_to_skel_meshpath(loc_mesh,full_cell_skeleton,pt,filterpts)
+
+    assert(len(path)==17)
+    assert(path[0] == 970)
+    assert(path[6] == 960)
 
 
