75902d894da7142cb5bf57cc60d06cad5b213142
Sharmi <shtaaa@gmail.com>
2019-08-30 10:49:54 -0700

    adding filter option

--- meshparty/mesh_skel_utils.py
+++ meshparty/mesh_skel_utils.py
@@ -4,18 +4,30 @@ import trimesh
 from . import utils
 
 
-def point_to_skel_meshpath(mesh,sk,pt):
+def intersection(lst1, lst2): 
+    return list(set(lst1) & set(lst2)) 
+
+
+def point_to_skel_meshpath(mesh,sk,pt,filter=None):
     '''
 
     Given a mesh, a skeleton and a point,  find the path along the mesh between a point on the mesh and the skeleton (closest skeleton point). If the point is not on the mesh, the point used is the mesh vertex that is closest to the point
     :param mesh: Trimesh-like mesh with N vertices
     :param sk: a skeleton whose vertices are a subset of the vertices of mesh
     :param pt : a length 3 array specifying a point location
-    
+    :param filter: a boolean array which is the filter that was used to generate "mesh", 
+     if "mesh" is a filtered version of the mesh that was used to generate the skeleton "sk"
     '''
 
-    t = sk.vertex_properties['mesh_index']
-    sk_inds = [val for i,val in enumerate(t) if not val == -1 ]
+
+    if filter is None:
+    	t = sk.vertex_properties['mesh_index']
+    	sk_inds = [val for i,val in enumerate(t) if not val == -1 ]
+    else:
+	validinds = np.where(filtpts)[0]
+        localskeletoninds = list(set(sk.vertex_properties['mesh_index']) & set(validinds)) #intersection of validinds and sk.vertex_properties['mesh_index']
+	sk_inds = [i for i, val in enumerate(validinds) if val in localskeletoninds]
+
 
     if len(sk_inds) < 1:
         return None
