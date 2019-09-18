cf7e11f41149e3e88656f97dc0d664f4e640739b
Forrest Collman <forrest.collman@gmail.com>
2019-09-18 15:16:40 -0700

    added comments

--- meshparty/trimesh_vtk.py
+++ meshparty/trimesh_vtk.py
@@ -821,17 +821,25 @@ def scale_bar_actor(center, camera, length=10000, color=(0,0,0), linewidth=5, fo
     axes_actor.SetBounds(center[0], center[0]+length,
                          center[1], center[1]+length,
                          center[2], center[2]+length)
+    # this means no real labels
     axes_actor.SetLabelFormat("")
     axes_actor.SetCamera(camera)
+    # this turns off the tick marks and labelled numbers
     axes_actor.SetNumberOfLabels(0)
+    # this affects whether the corner of the 3 axis
+    # changes as you rotate the view
+    # this option makes it stay constant
     axes_actor.SetFlyModeToNone()
     axes_actor.SetFontFactor(1.0)
     axes_actor.GetProperty().SetColor(*color)
     axes_actor.GetProperty().SetLineWidth(linewidth)
+    # this controls the color of text
     tprop =vtk.vtkTextProperty()
     tprop.SetColor(*color)
+    # no shadows on text
     tprop.ShadowOff()
     tprop.SetFontSize(font_size)
+    # makes the xyz and labels the same
     axes_actor.SetAxisTitleTextProperty(tprop)
     axes_actor.SetAxisLabelTextProperty(tprop)
 
