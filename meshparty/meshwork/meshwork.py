50784d7111aa334ac2de276e7209d8727b6895dc
Forrest Collman <forrest.collman@gmail.com>
2023-06-14 06:58:25 -0700

    remove vtk from meshwork

--- meshparty/meshwork/meshwork.py
+++ meshparty/meshwork/meshwork.py
@@ -1,13 +1,6 @@
-try:
-    import trimesh_vtk
-
-    _vtk_loaded = True
-except:
-    _vtk_loaded = False
-
 from ..skeleton_io import swc_node_labels
 from ..trimesh_io import Mesh
-from ..skeleton import Skeleton, resample
+
 
 import contextlib
 import io
@@ -1502,42 +1495,6 @@ class Meshwork(object):
     # Visualization functions #
     ###########################
 
-    def requires_vtk(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if _vtk_loaded:
-                return func(*args, **kwargs)
-            else:
-                return None
-
-        return wrapper
-
-    @requires_vtk
-    def mesh_actor(self, **kwargs):
-        if self.mesh is not None:
-            return trimesh_vtk.mesh_actor(self.mesh, **kwargs)
-        else:
-            return None
-
-    @requires_vtk
-    def anno_point_actor(self, anno_name, query=None, filter_query=None, **kwargs):
-        row_filter = np.full(len(self.anno[anno_name].df), True)
-        if query is not None:
-            qry = self.anno[anno_name].query(query)
-            row_filter = np.logical_and(row_filter, qry.row_filter)
-        if filter_query is not None:
-            qry = self.anno[anno_name].filter_query(filter_query)
-            row_filter = np.logical_and(row_filter, qry.row_filter)
-        if anno_name in self.anno.table_names:
-            return trimesh_vtk.point_cloud_actor(
-                self.anno[anno_name].points[row_filter], **kwargs
-            )
-
-    @OnlyIfSkeleton.exists
-    @requires_vtk
-    def skeleton_actor(self, **kwargs):
-        if self.skeleton is not None:
-            return trimesh_vtk.skeleton_actor(self.skeleton, **kwargs)
 
     @property
     @OnlyIfSkeleton.exists
